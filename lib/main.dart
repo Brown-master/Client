7651994d5e1db8637506d93b9d88fb0962dcb06e
newbiejh <dlwjdgns301@naver.com>
2022-11-12 18:34:44 +0900

    ✨ Firebase 설정

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:ui_repository/pages/home_page.dart';
 import 'package:ui_repository/pages/login_page.dart';
 import 'package:ui_repository/pages/register_page.dart';
 import 'package:ui_repository/pages/info_page.dart';
+import 'components/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
