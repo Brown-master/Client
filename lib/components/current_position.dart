c3709966cff75228c58cbb0d48a8f9c64dbaac62
newbiejh <dlwjdgns301@naver.com>
2022-12-04 23:17:57 +0900

    🔥 현재 위치 좌표 구하기 파일 삭제

--- lib/components/current_position.dart
+++
@@ -1,9 +0,0 @@
-import 'package:geolocator/geolocator.dart';
-
-// 사용자 현재 위치 구하는 함수
-Future<Position> getCurrentLocation() async {
-  Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
-
-  return position;
-}
