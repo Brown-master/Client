b287e0776f985f5a5b429b30b324b3108d3111a3
newbiejh <dlwjdgns301@naver.com>
2022-12-04 23:19:05 +0900

    ✨ 사고 현황 정보 받아오기

--- lib/components/home_form.dart
+++ lib/components/home_form.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:ui_repository/components/reverse_geocoding.dart';
 import 'dart:convert';
 import 'dart:async';
 import 'url.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeForm extends StatefulWidget {
   const HomeForm({Key? key}) : super(key: key);
@@ -13,15 +15,31 @@ class HomeForm extends StatefulWidget {
 
 class _HomeFormState extends State<HomeForm> {
   var data = [];
+  var lat;
+  var lon;
+  var address;
 
   @override
   void initState() {
     super.initState();
-    this.fetchAccident();
+    address = getAddress();
+    getCurrentLocation();
   }
 
-  Future fetchAccident() async {
-    final response = await http.get(accidenturl);
+  Future<void> getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    //lat = position.latitude;
+    //lon = position.longitude;
+    lat = 37.5323;
+    lon = 127.1938;
+
+    fetchAccident(lat: position.latitude, lon: position.longitude);
+  }
+
+  Future fetchAccident({required lat, required lon}) async {
+    final response = await http
+        .get(Uri.parse('${accidenturl}latitude=$lat&longitude=$lon'));
 
     var list = [];
     if (response.statusCode == 200) {
@@ -30,10 +48,21 @@ class _HomeFormState extends State<HomeForm> {
     } else {
       throw Exception('Failed to load Accident');
     }
-
     setState(() {
       data = list;
     });
+  }
+
+  Future<Accident> getAddress() async {
+    final response = await http.get(Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=AIzaSyCxeCqslVuJkR-gmT2oObsMdgrr6vfdYsI&language='
+        'ko'
+        ''));
+    if (response.statusCode == 200) {
+      return Accident.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load Accident');
+    }
   }
 
   @override
@@ -55,88 +84,116 @@ class _HomeFormState extends State<HomeForm> {
         child: Scrollbar(
           thickness: 5.0, // 스크롤 너비
           radius: Radius.circular(8.0), // 스크롤 라운딩
-          child: ListView.builder(
-              itemCount: data == null ? 0 : data.length,
-              itemBuilder: (context, index) {
-                return Card(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: 50,
-                        ),
-                        Text('userId: ${data[index]['userId'].toString()}'),
-                        Text('id: ${data[index]['id'].toString()}'),
-                        Text('title: ${data[index]['title']}'),
-                        Text(
-                            'completed: ${data[index]['completed'].toString()}'),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            Navigator.pushNamed(context, '/info',
-                                arguments: AccidentInfo(
-                                    userId: data[index]['userId'],
-                                    id: data[index]['id'],
-                                    title: data[index]['title'],
-                                    completed: data[index]['completed']));
+          child: FutureBuilder<Accident>(
+              future: address,
+              builder: (context, snapshot) {
+                return ListView.builder(
+                    itemCount: data.isEmpty ? 1 : data.length,
+                    itemBuilder: (context, index) {
+                      if (data.isNotEmpty) {
+                        return Card(
+                            margin: EdgeInsets.all(10),
+                            color: Colors.white,
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 50,
+                                ),
+                                Text('사고 내용: ${data[index]['message']}'),
+                                Text('도로명: ${data[index]['road_name']}'),
+                                Text('도로번호: ${data[index]['road_direction']}'),
+                                Text(
+                                    '주소명: ${snapshot.data?.results?[0].formattedAddress}'),
+                                Text('사고 시각: ${data[index]['date_time']}'),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    Navigator.pushNamed(context, '/info',
+                                        arguments: AccidentInfo(
+                                          accident_id: data[index]
+                                              ['accident_id'],
+                                          message: data[index]['message'],
+                                          road_name: data[index]['road_name'],
+                                          road_num: data[index]['road_num'],
+                                          road_direction: data[index]
+                                              ['road_direction'],
+                                          latitude: data[index]['latitude'],
+                                          longitude: data[index]['longitude'],
+                                          date_time: data[index]['date_time'],
+                                        ));
 
-                            final delete = await http.delete(Uri.parse(
-                                '${accidenturl}/${data[index]['id']}'));
-                            final get = await http.get(accidenturl);
+                                    final delete = await http.delete(Uri.parse(
+                                        '${accidenturl}/${data[index]['accident_id']}'));
+                                    final get = await http.get(Uri.parse(
+                                        '${accidenturl}${lat}&${lon}'));
 
-                            var list = [];
-                            if (get.statusCode == 200) {
-                              String responseBody = utf8.decode(get.bodyBytes);
-                              list = jsonDecode(responseBody);
-                            } else {
-                              throw Exception('Failed to delete');
-                            }
+                                    var list = [];
+                                    if (get.statusCode == 200) {
+                                      String responseBody =
+                                          utf8.decode(get.bodyBytes);
+                                      list = jsonDecode(responseBody);
+                                    } else {
+                                      throw Exception('Failed to delete');
+                                    }
 
-                            setState(() {
-                              data = list;
-                            });
-                          },
-                          child: Text('수락'),
-                          style: TextButton.styleFrom(
-                              minimumSize: Size(150, 50),
-                              padding: EdgeInsets.all(16.0),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            final delete = await http.delete(Uri.parse(
-                                '${accidenturl}/${data[index]['id']}'));
-                            final get = await http.get(accidenturl);
+                                    setState(() {
+                                      data = list;
+                                    });
+                                  },
+                                  child: Text('수락'),
+                                  style: TextButton.styleFrom(
+                                      minimumSize: Size(150, 50),
+                                      padding: EdgeInsets.all(16.0),
+                                      tapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    final delete = await http.delete(Uri.parse(
+                                        '${accidenturl}/${data[index]['accident_id']}'));
+                                    final get = await http.get(Uri.parse(
+                                        '${accidenturl}${lat}&${lon}'));
 
-                            var list = [];
-                            if (get.statusCode == 200) {
-                              String responseBody = utf8.decode(get.bodyBytes);
-                              list = jsonDecode(responseBody);
-                            } else {
-                              throw Exception('Failed to delete');
-                            }
+                                    var list = [];
+                                    if (get.statusCode == 200) {
+                                      String responseBody =
+                                          utf8.decode(get.bodyBytes);
+                                      list = jsonDecode(responseBody);
+                                    } else {
+                                      throw Exception('Failed to delete');
+                                    }
 
-                            setState(() {
-                              data = list;
-                            });
-                          },
-                          child: Text('거절'),
-                          style: TextButton.styleFrom(
-                              minimumSize: Size(150, 50),
-                              padding: EdgeInsets.all(16.0),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                      ],
-                    ));
+                                    setState(() {
+                                      data = list;
+                                    });
+                                  },
+                                  child: Text('거절'),
+                                  style: TextButton.styleFrom(
+                                      minimumSize: Size(150, 50),
+                                      padding: EdgeInsets.all(16.0),
+                                      tapTargetSize:
+                                          MaterialTapTargetSize.shrinkWrap),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ));
+                      } else {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 300),
+                            Text('사고 현황 없음'),
+                          ],
+                        );
+                      }
+                    });
               }),
         ),
       ),
@@ -145,14 +202,22 @@ class _HomeFormState extends State<HomeForm> {
 }
 
 class AccidentInfo {
-  final int userId;
-  final int id;
-  final String title;
-  final bool completed;
+  final int accident_id;
+  final String message;
+  final String road_name;
+  final int road_num;
+  final String road_direction;
+  final double latitude;
+  final double longitude;
+  final String date_time;
 
   AccidentInfo(
-      {required this.userId,
-      required this.id,
-      required this.title,
-      required this.completed});
+      {required this.accident_id,
+      required this.message,
+      required this.road_name,
+      required this.road_num,
+      required this.road_direction,
+      required this.latitude,
+      required this.longitude,
+      required this.date_time});
 }
--- lib/components/url.dart
+++ lib/components/url.dart
@@ -1,2 +1,2 @@
-Uri accidenturl = Uri.parse('http://localhost:8000/posts');
-Uri historyurl = Uri.parse('http://localhost:8000/posts');
+String accidenturl = 'http://192.168.35.141:8080/accident/search?';
+Uri historyurl = Uri.parse('http://192.168.35.141:5000/posts');
